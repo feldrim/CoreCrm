212885e26f5452dd0330b55bc6afbc3c4e3eba56
the f <feldrim@gmail.com>
2018-02-23 22:53:06 +0300

    Modified test project

---
+++ Fiver.Security.AspIdentity.Tests/HomeControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Fiver.Security.AspIdentity.Controllers;
+using FluentAssertions.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fiver.Security.AspIdentity.Tests
+{
+   [TestClass]
+   public class HomeControllerTests
+   {
+      [TestMethod]
+      public void Index_Action_Returns_View()
+      {
+         // Arrange
+         var controller = new HomeController();
+
+         // Act
+         var result = controller.Index();
+
+         // Assert
+         result.Should().BeViewResult();
+      }
+   }
+}
