f2b3977a381eaf89343e8ca7af9509b528e944f1
the f <feldrim@gmail.com>
2018-02-24 15:03:44 +0300

    Added Serilog logging for both console and file.

--- Fiver.Security.AspIdentity/Program.cs
+++ Fiver.Security.AspIdentity/Program.cs
@@ -2,38 +2,59 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace Fiver.Security.AspIdentity
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+   public class Program
+   {
+      public static void Main(string[] args)
+      {
+         Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.RollingFile("log-{Date}.log", fileSizeLimitBytes: 1048576, shared: true)
+            .CreateLogger();
+
+         try
+         {
+            Log.Information("Starting web host");
             var webHost = BuildWebHost(args);
 
             using (var scope = webHost.Services.CreateScope())
             {
-                var services = scope.ServiceProvider;
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                try
-                {
-                    Seeder.Initialize(services);
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
+               var services = scope.ServiceProvider;
+               try
+               {
+                  Seeder.Initialize(services);
+               }
+               catch (Exception ex)
+               {
+                  Log.Error(ex, "An error occurred seeding the DB.");
+               }
             }
 
             webHost.Run();
-        }
+         }
+         catch (Exception ex)
+         {
+            Log.Fatal(ex, "Host terminated unexpectedly");
+         }
+         finally
+         {
+            Log.CloseAndFlush();
+         }
+      }
 
-        public static IWebHost BuildWebHost(string[] args)
-        {
-            return WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-        }
-    }
+      public static IWebHost BuildWebHost(string[] args)
+      {
+         return WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .UseSerilog()
+            .Build();
+      }
+   }
 }
